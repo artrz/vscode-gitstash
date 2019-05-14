e8774e452f44a949683581f4ec20c05cd6803bb2
Nick Popov <nckcol@gmail.com>
2019-05-14 16:00:04 +0300

    fix: remove duplicate repositories
    prevents repository duplicating if vscode
    workspaces has common repository(-ies)

--- src/Git.ts
+++ src/Git.ts
@@ -89,7 +89,9 @@ export default class Git {
                 continue;
             }
 
-            paths.push(gitPath);
+            if(paths.indexOf(gitPath) === -1) {
+                paths.push(gitPath);
+            }
 
             if (firstOnly) {
                 break;
